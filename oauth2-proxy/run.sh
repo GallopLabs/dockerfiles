00ce62dafe027e3c0592bc7d3d72e98822d3420f
Jonathon W. Marshall <jonathon@gallop.io>
2015-08-10 00:34:33 -0400

    include cookie domain

--- oauth2-proxy/run.sh
+++ oauth2-proxy/run.sh
@@ -16,15 +16,17 @@ if [ -z $COOKIE_SECRET ]; then
   exit 1
 fi
 
-export EMAIL_DOMAIN=${EMAIL_DOMAIN:-gallop.io}
 export UPSTREAM=${UPSTREAM:-}
 export REDIRECT_URL=${REDIRECT_URL:-}
+export EMAIL_DOMAIN=${EMAIL_DOMAIN:-gallop.io}
+export COOKIE_DOMAIN=${COOKIE_DOMAIN:-.gallop.io}
 
 exec /usr/local/bin/oauth2_proxy \
   -http-address=0.0.0.0:4180 \
   -https-address=0.0.0.0:443 \
   -upstream=${UPSTREAM} \
   -redirect-url=${REDIRECT_URL} \
+  -cookie-domain=${COOKIE_DOMAIN} \
   -cookie-secret=${COOKIE_SECRET} \
   -client-id=${CLIENT_ID} \
   -client-secret ${CLIENT_SECRET} \

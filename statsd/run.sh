8367e2db71a4cf949c584a75df7285eb31a7e851
Jonathon W. Marshall <jonathon@gallop.io>
2015-08-19 19:59:43 -0400

    convert to alpine linux

--- statsd/run.sh
+++ statsd/run.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -e
 
 DEBUG=${DEBUG:-false}
 DUMP_MESSAGES=${DUMP_MESSAGES:-false}
+FLUSH_INTERVAL=${FLUSH_INTERVAL:-600}
+REPEATER_PORT=${REPEATER_PORT:-8125}
 
 if [ ! -z $GRAPHITE_PORT_2003_TCP_ADDR ]; then
   export GRAPHITE_HOST="${GRAPHITE_PORT_2003_TCP_ADDR}"
@@ -11,13 +14,18 @@ if [ ! -z $GRAPHITE_PORT_2003_TCP_PORT ]; then
   export GRAPHITE_PORT="${GRAPHITE_PORT_2003_TCP_PORT}"
 fi
 
-if [ ! -z $GRAPHITE_HOST ]; then
+case $1 in
+graphite)
+  if [ -z $GRAPHITE_HOST ]; then
+    echo "Error: You must specify the GRAPHITE_HOST or link this image to a graphite container" >&2
+    exit 1
+  fi
   cat > /opt/statsd/config.js <<EOF
 {
   "port": 8125,
-  "flushInterval": 60000,
   "debug": $DEBUG,
   "dumpMessages": $DUMP_MESSAGES,
+  "flushInterval": $FLUSH_INTERVAL,
   "backends": [
     "./backends/graphite"
   ],
@@ -25,14 +33,41 @@ if [ ! -z $GRAPHITE_HOST ]; then
   "graphitePort": $GRAPHITE_PORT
 }
 EOF
-else
+  ;;
+repeater)
+  if [ -z $REPEATER_HOST ]; then
+    echo "Error: You must specify the REPEATER_HOST"
+    exit 1
+  fi
+
   cat > /opt/statsd/config.js <<EOF
 {
   "port": 8125,
   "debug": $DEBUG,
-  "dumpMessages": $DUMP_MESSAGES
+  "dumpMessages": $DUMP_MESSAGES,
+  "flushInterval": $FLUSH_INTERVAL,
+  "backends": [
+    "./backends/repeater"
+  ],
+  "repeater": [
+    {
+      host: "$REPEATER_HOST",
+      port: $REPEATER_PORT
+    }
+  ]
 }
 EOF
-fi
+  ;;
+*)
+  cat > /opt/statsd/config.js <<EOF
+{
+  "port": 8125,
+  "debug": $DEBUG,
+  "dumpMessages": $DUMP_MESSAGES,
+  "flushInterval": $FLUSH_INTERVAL
+}
+EOF
+ ;;
+esac
 
-statsd /opt/statsd/config.js
+exec statsd /opt/statsd/config.js
